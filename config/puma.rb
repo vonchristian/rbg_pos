e4d2a7fde1ca8fbaaedb6970321a97dfa8a6ce9a
Von Christian Halip <vc.halip@gmail.com>
2017-08-02 12:49:34 +0800

    Update

--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,14 @@
-# workers 2
-# threads 2,8
+workers 2
+threads 2,8
 
-# environment 'production'
-# directory '/var/www/paenro/current'
-# shared_dir = File.expand_path("../..", __FILE__)
+environment 'production'
+directory '/var/www/paenro/current'
+shared_dir = File.expand_path("../..", __FILE__)
 
-# stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
-# rackup DefaultRackup
-# bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
-# pidfile "#{shared_dir}/tmp/pids/puma.pid"
+rackup DefaultRackup
+bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+pidfile "#{shared_dir}/tmp/pids/puma.pid"
 
-# prune_bundler
+prune_bundler
