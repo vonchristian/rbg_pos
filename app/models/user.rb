06eafc3be3e8dff7369dd7eeeaf0958c785acbc4
Von Christian Halip <vc.halip@gmail.com>
2017-08-10 16:25:21 +0800

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  belongs_to :branch
+  belongs_to :branch, optional: true
 end
