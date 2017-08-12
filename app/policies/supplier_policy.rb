dcc880634bc3bde10080c9d2391cac1937faf3c9
Von Christian Halip <vc.halip@gmail.com>
2017-08-12 16:14:59 +0800

    Fix supplier policy issue

--- app/policies/supplier_policy.rb
+++ app/policies/supplier_policy.rb
@@ -2,4 +2,10 @@ class SupplierPolicy < ApplicationPolicy
 	def index?
 		user.proprietor? || user.stock_custodian?
 	end 
+	def new?
+		user.proprietor? || user.stock_custodian?
+	end
+	def create?
+		new?
+	end
 end 
