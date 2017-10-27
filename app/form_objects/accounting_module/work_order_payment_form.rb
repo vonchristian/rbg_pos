ec640056f76da25f090ee36f612d2c8a6334e8d6
Von Christian Halip <vc.halip@gmail.com>
2017-10-27 13:16:23 +0800

    Update

--- app/form_objects/accounting_module/work_order_payment_form.rb
+++ app/form_objects/accounting_module/work_order_payment_form.rb
@@ -15,19 +15,20 @@ module AccountingModule
     def save 
       ActiveRecord::Base.transaction do 
         create_entry 
+        create_order
       end 
     end 
 
     private 
     def create_entry 
-      if discount_amount.nil?
-        AccountingModule::Entry.work_order_payment.create!(user_id: user_id, commercial_document_id: work_order_id, commercial_document_type: "WorkOrder", entry_date: entry_date, reference_number: reference_number, description: description,
+      if discount_amount.to_f.zero?
+        AccountingModule::Entry.work_order_payment.create!(recorder_id: user_id, commercial_document_id: work_order_id, commercial_document_type: "WorkOrder", entry_date: entry_date, reference_number: reference_number, description: description,
         credit_amounts_attributes: [amount: amount, account_id: credit_account_id],
         debit_amounts_attributes: [amount: amount, account_id: debit_account_id])
-      elsif discount_amount
-        AccountingModule::Entry.work_order_payment.create!(user_id: user_id, commercial_document_id: work_order_id, commercial_document_type: "WorkOrder", entry_date: entry_date, reference_number: reference_number, description: description,
-        credit_amounts_attributes:  [{amount: amount, account_id: credit_account_id}, { amount: discount_amount, account_id: discount_credit_account_id }],
-        debit_amounts_attributes: [{amount: amount, account_id: debit_account_id}, {amount: discount_amount, account_id: discount_debit_account_id}])
+      elsif discount_amount.to_f > 0
+        AccountingModule::Entry.work_order_payment.create!(recorder_id: user_id, commercial_document_id: work_order_id, commercial_document_type: "WorkOrder", entry_date: entry_date, reference_number: reference_number, description: description,
+        credit_amounts_attributes:  [{amount: (amount.to_f + discount_amount.to_f), account_id: credit_account_id}],
+        debit_amounts_attributes: [{amount: amount.to_f, account_id: debit_account_id}, {amount: discount_amount.to_f, account_id: discount_debit_account_id}])
       end
     end 
     def discount_debit_account_id
@@ -35,6 +36,10 @@ module AccountingModule
     end 
     def discount_credit_account_id
       AccountingModule::Asset.find_by(name: "Cash on Hand (Cashier)").id
+    end
+     def create_order
+      order = Order.create!(description: "payment of service ##{WorkOrder.find_by(id: work_order_id).service_number}", customer_id: WorkOrder.find_by(id: work_order_id).customer_id, date: entry_date, employee_id: user_id)
+      Payment.create(mode_of_payment: 'cash', order: order, discount_amount: discount_amount, total_cost: amount)
     end
   end 
 end
