de8588c19404996e2409e8a1e3b71815a95da334
Von Christian Halip <vc.halip@gmail.com>
2018-02-24 10:05:03 +0800

    Updates

--- app/models/store_front_module/orders/work_order.rb
+++ app/models/store_front_module/orders/work_order.rb
@@ -1,6 +1,6 @@
 module StoreFrontModule
   module Orders
-    class WorkOrder
+    class WorkOrder < Order
       has_many :work_order_line_items, class_name: "StoreFrontModule::LineItems::WorkOrderLineItem"
     end
   end
