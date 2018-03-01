4345d66c0fbec6e33e4d7baac463a3a5d0b480e9
Von Christian Halip <vc.halip@gmail.com>
2018-03-01 21:37:14 +0800

    Updates

--- app/controllers/store_front_module/orders/purchase_orders_controller.rb
+++ app/controllers/store_front_module/orders/purchase_orders_controller.rb
@@ -6,6 +6,7 @@ module StoreFrontModule
       end
       def show
         @order = StoreFrontModule::Orders::PurchaseOrder.find(params[:id])
+        @line_items = @order.purchase_order_line_items.order(created_at: :asc).paginate(page: params[:page], per_page: 35)
       end
     end
   end
