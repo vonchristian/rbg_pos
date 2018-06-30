f9181b4eb811756e9ca34e2c57aeba15bbfada93
Von Christian Halip <vc.halip@gmail.com>
2018-06-30 16:05:33 +0800

    Updates

--- app/models/store_front_module/line_items/referenced_purchase_order_line_item.rb
+++ app/models/store_front_module/line_items/referenced_purchase_order_line_item.rb
@@ -2,7 +2,7 @@ module StoreFrontModule
   module LineItems
     class ReferencedPurchaseOrderLineItem < LineItem
       belongs_to :sales_order_line_item, optional: true
-      belongs_to :purchase_order_line_item, class_name: "StoreFrontModule::LineItems::ReferencedPurchaseOrderLineItem", optional: true
+      belongs_to :purchase_order_line_item, class_name: "StoreFrontModule::LineItems::PurchaseOrderLineItem", optional: true
       delegate :purchase_cost, to: :purchase_order_line_item
 
       def self.cost_of_goods_sold
