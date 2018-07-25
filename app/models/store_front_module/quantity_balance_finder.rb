1f88289122f1ea0dee96a2bbd00f62659221de49
Von Christian Halip <vc.halip@gmail.com>
2018-07-25 11:09:17 +0800

    Updates

--- app/models/store_front_module/quantity_balance_finder.rb
+++ app/models/store_front_module/quantity_balance_finder.rb
@@ -3,9 +3,9 @@ module StoreFrontModule
     def balance(options={})
       if options[:from_date].present? && options[:to_date].present?
         date_range = DateRange.new(from_date: from_date, to_date: to_date)
-        joins(:order).where.not(order_id: nil).where('orders.date' => (date_range.start_date)..(date_range.end_date)).sum(&:converted_quantity)
+        joins(:order).where('orders.date' => (date_range.start_date)..(date_range.end_date)).where.not(order_id: nil).sum(&:converted_quantity)
       elsif options[:product_id].present?
-        joins(:product).where.not(order_id: nil).where('product_id' => options[:product_id]).sum(&:converted_quantity)
+        joins(:product).where('product_id' => options[:product_id]).where.not(order_id: nil).sum(&:converted_quantity)
       else
         joins(:order).where.not(order_id: nil).sum(&:converted_quantity)
       end
