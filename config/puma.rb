25a8ecfecd6e3795c72463ef202badac41c7de01
Von Christian Halip <vc.halip@gmail.com>
2019-01-15 14:37:27 +0800

    Updates

--- config/puma.rb
+++ config/puma.rb
@@ -1,18 +1,18 @@
-workers 3
-threads 5,5
-
-environment 'production'
-directory '/var/www/rbg_pos/current'
-shared_dir = File.expand_path("../..", __FILE__)
-
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-
-rackup DefaultRackup
-bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
-pidfile "#{shared_dir}/tmp/pids/puma.pid"
-
-prune_bundler
-preload_app!
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
-end
+# workers 3
+# threads 5,5
+#
+# environment 'production'
+# directory '/var/www/rbg_pos/current'
+# shared_dir = File.expand_path("../..", __FILE__)
+#
+# stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+#
+# rackup DefaultRackup
+# bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+# pidfile "#{shared_dir}/tmp/pids/puma.pid"
+#
+# prune_bundler
+# preload_app!
+# on_worker_boot do
+#   ActiveRecord::Base.establish_connection
+# end
