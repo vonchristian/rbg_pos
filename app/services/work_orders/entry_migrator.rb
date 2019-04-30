f2219c68bf8d793c0f8a5a6fc886eff3bbf1f701
Von Christian <vc.halip@gmail.com>
2019-04-30 21:07:58 +0800

    add entry migrator task

--- app/services/work_orders/entry_migrator.rb
+++ app/services/work_orders/entry_migrator.rb
@@ -6,7 +6,12 @@ module WorkOrders
       @work_order = args.fetch(:work_order)
     end
 
-    def migrate_receivable_account_entries!
+    def migrate_entries!
+      migrate_receivable_account_entries
+      migrate_sales_account_entries
+    end 
+
+    def migrate_receivable_account_entries
       work_order.service_charges.each do |charge|
         StoreFront.receivable_accounts.each do |account|
           account.debit_amounts.where(commercial_document: charge).each do |amount|
@@ -15,7 +20,7 @@ module WorkOrders
         end
       end
     end
-    def migrate_sales_account_entries!
+    def migrate_sales_account_entries
       StoreFront.sales_accounts.each do |account|
         account.credit_amounts.where(commercial_document: work_order).each do |amount|
           amount.update(account: work_order.sales_revenue_account)
--- lib/tasks/work_order_account.rake
+++ lib/tasks/work_order_account.rake
@@ -5,4 +5,9 @@ namespace :work_orders do
       AccountCreators::WorkOrder.new(work_order: work_order).create_accounts!
     end
   end
+  task migrate_entries: :environment do
+    WorkOrder.all.each do |work_order|
+      WorkOrders::EntryMigrator.new(work_order: work_order).migrate_entries!
+    end
+  end
 end
