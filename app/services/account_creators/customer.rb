ba2e8c437076297eecd729b8643e2c4e46baefe5
Von Christian <vc.halip@gmail.com>
2019-04-30 20:11:49 +0800

    updates

--- app/services/account_creators/customer.rb
+++ app/services/account_creators/customer.rb
@@ -13,36 +13,44 @@ module AccountCreators
     end
 
     def create_receivable_account
-      account = AccountingModule::Asset.create!(
+      if customer.receivable_account.blank?
+        account = AccountingModule::Asset.create!(
         business:     business,
         name:         receivable_account_name,
         account_code: SecureRandom.uuid)
-      customer.update(receivable_account: account)
+        customer.update(receivable_account: account)
+      end
     end
 
     def create_sales_revenue_account
-      account = AccountingModule::Revenue.create!(
+      if customer.sales_revenue_account.blank?
+        account = AccountingModule::Revenue.create!(
         business:     business,
         name:         sales_revenue_account_name,
         account_code: SecureRandom.uuid)
-      customer.update(sales_revenue_account: account)
+        customer.update(sales_revenue_account: account)
+      end
     end
 
     def create_sales_discount_account
-      account = AccountingModule::Revenue.create!(
+      if customer.sales_discount_account.blank?
+        account = AccountingModule::Revenue.create!(
         contra:      true,
         business:     business,
         name:         sales_discount_account_name,
         account_code: SecureRandom.uuid)
-      customer.update(sales_discount_account: account)
+        customer.update(sales_discount_account: account)
+      end
     end
 
     def create_service_revenue_account
-      account = AccountingModule::Revenue.create!(
+      if customer.service_revenue_account.blank?
+        account = AccountingModule::Revenue.create!(
         business:     business,
         name:         service_revenue_account_name,
         account_code: SecureRandom.uuid)
-      customer.update(service_revenue_account: account)
+        customer.update(service_revenue_account: account)
+      end 
     end
 
     def receivable_account_name
