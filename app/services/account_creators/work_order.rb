383dfa57c76dc98a129b73d818d099c32025d1c0
Von Christian <vc.halip@gmail.com>
2019-04-30 20:25:02 +0800

    Updates

--- app/services/account_creators/work_order.rb
+++ app/services/account_creators/work_order.rb
@@ -16,40 +16,48 @@ module AccountCreators
     private
 
     def create_receivable_account
-      account = AccountingModule::Asset.create!(
+      if work_order.receivable_account.blank?
+        account = AccountingModule::Asset.create!(
         business:     business,
         name:         receivable_account_name,
         account_code: SecureRandom.uuid)
-      work_order.update(receivable_account: account)
-      customer.receivable_account.sub_accounts.create(sub_account: account)
+        work_order.update(receivable_account: account)
+        customer.receivable_account.sub_accounts.create(sub_account: account)
+      end
     end
 
     def create_sales_revenue_account
-      account = AccountingModule::Revenue.create!(
+      if sales_revenue_account.blank?
+        account = AccountingModule::Revenue.create!(
         business:     business,
         name:         sales_revenue_account_name,
         account_code: SecureRandom.uuid)
-      work_order.update(sales_revenue_account: account)
-      customer.sales_revenue_account.sub_accounts.create(sub_account: account)
+        work_order.update(sales_revenue_account: account)
+        customer.sales_revenue_account.sub_accounts.create(sub_account: account)
+      end
     end
 
     def create_sales_discount_account
-      account = AccountingModule::Revenue.create!(
+      if work_order.sales_discount_account.blank?
+        account = AccountingModule::Revenue.create!(
         contra:       true,
         business:     business,
         name:         sales_discount_account_name,
         account_code: SecureRandom.uuid)
-      work_order.update(sales_discount_account: account)
-      customer.sales_discount_account.sub_accounts.create(sub_account: account)
+        work_order.update(sales_discount_account: account)
+        customer.sales_discount_account.sub_accounts.create(sub_account: account)
+      end
     end
 
     def create_service_revenue_account
-      account = AccountingModule::Revenue.create!(
+      if work_order.service_revenue_account.blank?
+        account = AccountingModule::Revenue.create!(
         business:     business,
         name:         service_revenue_account_name,
         account_code: SecureRandom.uuid)
-      work_order.update(service_revenue_account: account)
-      customer.service_revenue_account.sub_accounts.create(sub_account: account)
+        work_order.update(service_revenue_account: account)
+        customer.service_revenue_account.sub_accounts.create(sub_account: account)
+      end
     end
 
     def receivable_account_name
--- lib/tasks/work_order_account.rake
+++ lib/tasks/work_order_account.rake
@@ -2,19 +2,7 @@ namespace :work_orders do
   desc "Create work order accounts"
   task create_account: :environment do
     WorkOrder.all.each do |work_order|
-      AccountCreators::WorkOrder.new(work_order: work_order).create_account!
-    end
-  end
-
-  task migrate_service_charge_entries: :environment do
-    WorkOrder.all.each do |work_order|
-      WorkOrders::EntryMigration.new(work_order: work_order).migrate_service_charge_entries!
-    end
-  end
-
-  task migrate_spare_part_entries: :environment do
-    WorkOrder.all.each do |work_order|
-      WorkOrders::EntryMigration.new(work_order: work_order).migrate_spare_part_entries!
+      AccountCreators::WorkOrder.new(work_order: work_order).create_accounts!
     end
   end
 end
