490e0372f216c39594d6f4fbd8064c212357d4b7
Von Christian <vc.halip@gmail.com>
2019-05-08 08:15:50 +0800

    Updates

--- app/pdfs/reports/sales_pdf.rb
+++ app/pdfs/reports/sales_pdf.rb
@@ -70,7 +70,7 @@ module Reports
     def orders_data
       if @employee.present?
         [["DATE", "OR", "CUSTOMER", "ITEMS", "DISCOUNT", "TOTAL COST"]] +
-        @orders_data ||= @employee.sales_orders.ordered_on(from_date: (@from_date.beginning_of_day), to_date: @to_date.end_of_day).map{|o| [
+        @orders_data ||= @orders.map{|o| [
           o.date.strftime("%B %e, %Y"),
           o.reference_number,
           o.commercial_document.try(:name).try(:upcase),
