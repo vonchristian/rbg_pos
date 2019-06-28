786d5d6fccec3103ccc2951e662b0089290cc8ae
Von Christian <vc.halip@gmail.com>
2019-06-28 16:00:40 +0800

    updates

--- app/models/order.rb
+++ app/models/order.rb
@@ -79,7 +79,7 @@ class Order < ApplicationRecord
 
 
   def line_items_name
-    line_items.map{|a| ["#{a.product_name} (#{atry(:bar_code)})"] }.to_s.gsub("[", "").gsub("]", "").gsub('"', "")
+    line_items.map{|a| ["#{a.product_name} (#{a.try(:bar_code)})"] }.to_s.gsub("[", "").gsub("]", "").gsub('"', "")
   end
 
   def line_items_name_and_barcode
