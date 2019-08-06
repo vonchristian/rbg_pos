0e61a7500781a2a1feaebdef89bce281a7f448de
Von Christian <vc.halip@gmail.com>
2019-08-06 08:52:12 +0800

    updates

--- app/services/store_fronts/sales_migrator.rb
+++ app/services/store_fronts/sales_migrator.rb
@@ -8,9 +8,9 @@ module StoreFronts
 
     def migrate_sales!
       sales_order_line_items.each do |line_item|
-        # if line_item.stock.blank? && line_item.purchase_order_line_items.count == 1
+        if line_item.stock.blank? && line_item.purchase_order_line_items.count == 1
           line_item.update_attributes!(stock: line_item.purchase_order_line_items.last.stock)
-        # end
+        end
       end
     end
   end
--- app/services/store_fronts/stock_migrator.rb
+++ app/services/store_fronts/stock_migrator.rb
@@ -4,17 +4,17 @@ module StoreFronts
 
     def initialize(purchase_order_line_item:)
       @purchase_order_line_item = purchase_order_line_item
-      @order       = @purchase_order_line_item.purchase_order
-      @store_front = @order.store_front
+      @order                    = @purchase_order_line_item.purchase_order
+      @store_front              = @order.store_front
     end
 
     def create_stock
       if purchase_order_line_item.stock.blank?
         stock = store_front.stocks.create!(
-        product:     purchase_order_line_item.product,
-        store_front: order.store_front,
+        product:             purchase_order_line_item.product,
+        store_front:         order.store_front,
         unit_of_measurement: purchase_order_line_item.unit_of_measurement,
-        barcode:     purchase_order_line_item.bar_code)
+        barcode:             purchase_order_line_item.bar_code)
         purchase_order_line_item.update_attributes!(stock: stock)
       end
     end
