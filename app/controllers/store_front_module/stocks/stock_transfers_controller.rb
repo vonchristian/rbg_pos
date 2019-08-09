3b81682ccb9f24f13d132d4754e71529b40a9d18
Von Christian <vc.halip@gmail.com>
2019-08-09 14:41:41 +0800

    updates

--- app/controllers/store_front_module/stocks/stock_transfers_controller.rb
+++ app/controllers/store_front_module/stocks/stock_transfers_controller.rb
@@ -3,7 +3,7 @@ module StoreFrontModule
     class StockTransfersController < ApplicationController
       def index
         @stock = current_store_front.stocks.find(params[:stock_id])
-        @pagy, @stock_transfers = pagy(@stock.stock_transfers)
+        @pagy, @stock_transfers = pagy(@stock.stock_transfers.processed)
       end
     end
   end
