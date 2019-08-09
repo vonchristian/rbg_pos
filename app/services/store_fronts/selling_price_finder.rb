507ee50df6cb0e385a13a1e10069bd0392003429
Von Christian <vc.halip@gmail.com>
2019-08-09 11:11:01 +0800

    updates

--- app/services/store_fronts/selling_price_finder.rb
+++ app/services/store_fronts/selling_price_finder.rb
@@ -1,11 +1,11 @@
 module StoreFronts
   class SellingPriceFinder
-    attr_reader :selling_prices, :store_front
+    attr_reader :selling_prices, :store_front, :stock
     def initialize(stock:, store_front:)
       @stock               = stock
       @store_front         = store_front
       @unit_of_measurement = @stock.unit_of_measurement
-      @selling_prices      = @unit_of_measurement.selling_prices 
+      @selling_prices      = @unit_of_measurement.selling_prices
     end
 
     def selling_price
@@ -13,7 +13,7 @@ module StoreFronts
       if price
         price.price
       else
-        0
+        stock.product.try(:base_measurement).try(:price)
       end
     end
   end
