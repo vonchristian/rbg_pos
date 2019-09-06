76da5a178d0241b4bed00ac463b8d756cd69c0d5
Von Christian <vc.halip@gmail.com>
2019-09-06 11:17:27 +0800

    updates

--- app/models/store_front_module/line_items/sales_order_line_item.rb
+++ app/models/store_front_module/line_items/sales_order_line_item.rb
@@ -6,12 +6,14 @@ module StoreFrontModule
       has_many :purchase_order_line_items, through: :referenced_purchase_order_line_items, class_name: 'StoreFrontModule::LineItems::PurchaseOrderLineItem'
       has_many :referenced_sales_order_line_items, class_name: "StoreFrontModule::LineItems::ReferencedSalesOrderLineItem", foreign_key: 'sales_order_line_item_id'
       delegate :customer, :date, :official_receipt_number, :reference_number, :customer_name, to: :sales_order, allow_nil: true
+      delegate :purchase, to: :stock
+      delegate :unit_cost, to: :purchase, prefix: true
       def self.cost_of_goods_sold
         sum(&:cost_of_goods_sold)
       end
 
       def cost_of_goods_sold
-        quantity * unit_cost
+        quantity * purchase_unit_cost
       end
 
       def available_quantity
