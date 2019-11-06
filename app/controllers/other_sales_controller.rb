e57a9e9cb5a04d3c13899e0033a1e22b9d26531d
Von Christian <vc.halip@gmail.com>
2019-11-06 15:24:16 +0800

    updates

--- app/controllers/other_sales_controller.rb
+++ app/controllers/other_sales_controller.rb
@@ -5,15 +5,28 @@ class OtherSalesController < ApplicationController
   def create
     @other_sale = OtherSalesForm.new(other_sale_params)
     if @other_sale.valid?
-      @other_sale.process!
-      redirect_to store_index_url, notice: "Other Sales saved successfully"
+      ActiveRecord::Base.transaction do
+        @other_sale.process!
+        create_voucher(@other_sale.find_order)
+        create_entry(@other_sale.find_order)
+        redirect_to store_index_url, notice: "Other Sales saved successfully"
+      end
     else
       render :new
     end
   end
 
   private
+
   def other_sale_params
-    params.require(:other_sales_form).permit(:recorder_id, :amount, :reference_number, :description, :date, :recorder_id, :customer_id)
+    params.require(:other_sales_form).permit(:recorder_id, :amount, :reference_number, :description, :date, :recorder_id, :customer_id, :account_number)
+  end
+
+  def create_voucher(order)
+    Vouchers::OtherSalesOrderVoucher.new(order: order, employee: order.employee, amount: params[:other_sales_form][:amount]).create_voucher!
+  end
+
+  def create_entry(order)
+    VoucherEntryCreation.new(voucher: order.voucher).create_entry!
   end
 end
--- app/forms/other_sales_form.rb
+++ app/forms/other_sales_form.rb
@@ -1,7 +1,11 @@
 class OtherSalesForm
   include ActiveModel::Model
-  attr_accessor :description, :reference_number, :recorder_id, :amount, :date, :customer_id, :sales_order_id
+  attr_accessor :description, :reference_number, :recorder_id, :amount, :date, :customer_id, :account_number, :sales_order_id
   validates :amount, :description, :customer_id, :date, presence: true
+
+  def find_order
+    StoreFrontModule::Orders::SalesOrder.find_by(account_number: account_number)
+  end
 
   def process!
     ActiveRecord::Base.transaction do
@@ -18,13 +22,10 @@ class OtherSalesForm
       employee_id: recorder_id,
       reference_number: reference_number,
       store_front: find_employee.store_front,
-      account_number: SecureRandom.uuid)
+      account_number: account_number)
       create_accounts(order)
     order.save!
     order.create_cash_payment(cash_tendered: amount)
-
-    create_voucher(order)
-    create_entry(order)
   end
 
 
@@ -36,13 +37,7 @@ class OtherSalesForm
     User.find(recorder_id)
   end
 
-  def create_voucher(order)
-    Vouchers::OtherSalesOrderVoucher.new(order: order, employee: find_employee, amount: amount).create_voucher!
-  end
 
-  def create_entry(order)
-    VoucherEntryCreation.new(voucher: order.voucher).create_entry!
-  end
   def create_accounts(order)
     AccountCreators::SalesOrder.new(sales_order: order).create_accounts!
   end
