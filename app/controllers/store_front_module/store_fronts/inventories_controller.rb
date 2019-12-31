fb2e3eaa5148f89f00e80ba955ad725ccad7abcc
Von Christian <vc.halip@gmail.com>
2019-12-31 13:39:41 +0800

    updates

--- app/controllers/store_front_module/store_fronts/inventories_controller.rb
+++ app/controllers/store_front_module/store_fronts/inventories_controller.rb
@@ -12,40 +12,40 @@ module StoreFrontModule
         end
       end
 
-      private 
+      private
       def render_csv
         # Tell Rack to stream the content
         headers.delete("Content-Length")
-    
+
         # Don't cache anything from this generated endpoint
         headers["Cache-Control"] = "no-cache"
-    
+
         # Tell the browser this is a CSV file
         headers["Content-Type"] = "text/csv"
-    
+
         # Make the file download with a specific filename
         headers["Content-Disposition"] = "attachment; filename=\"Stock Inventory.csv\""
-    
+
         # Don't buffer when going through proxy servers
         headers["X-Accel-Buffering"] = "no"
-    
+
         # Set an Enumerator as the body
         self.response_body = csv_body
-  
+
         response.status = 200
       end
-    
+
       private
-    
+
       def csv_body
         Enumerator.new do |yielder|
           yielder << CSV.generate_line(["Name", "Barcode", "Purchases", "Sales", "Stock Transfers", "Spoilages", "Internal Uses", 'Sales Returns', 'For Warranty', "Available QTY"])
-          
+
           @stocks.joins(:product).order('products.name').each do |stock|
             yielder << CSV.generate_line([
-              stock.name, 
-              stock.barcode, 
-              stock.purchase_quantity, 
+              stock.name,
+              stock.barcode.to_s, 
+              stock.purchase_quantity,
               stock.sales_balance,
               stock.stock_transfers_balance,
               stock.spoilages_balance,
