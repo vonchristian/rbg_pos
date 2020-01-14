2d35058a799a5197255bbe744fe43c1997e8a3e1
Von Christian <vc.halip@gmail.com>
2020-01-14 08:29:38 +0800

    fix new work order feature

--- spec/system/work_orders/new_system_spec.rb
+++ spec/system/work_orders/new_system_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+include ChosenSelect
 
 describe 'New work order' do
   before(:each) do
@@ -6,15 +7,16 @@ describe 'New work order' do
     supplier   = create(:supplier, business_name: 'Test supplier')
     category   = create(:work_order_category, title: 'Desktop')
     customer   = create(:customer, first_name: 'Juan', last_name: 'Cruz')
-    user       = create(:user, role: 'technician')
+    user       = create(:user, role: 'technician', first_name: 'Test', last_name: 'Tech')
     login_as(user, scope: :user)
-    visit computer_repair_section_dashboard_index_url
-    click_link 'New Work Order'
+    visit computer_repair_section_dashboard_index_path
+    click_link 'New Repair'
   end
-  it 'with valid attributes' do
-    select 'Desktop'
+  it 'with valid attributes', js: true do
+    select_from_chosen 'Test Tech', from: 'Assigned To'
+    select_from_chosen 'Desktop', from: 'Category'
     fill_in 'Date received',            with: Date.current
-    select 'Juan Cruz'
+    select_from_chosen 'Juan Cruz', from: 'Customer'
     fill_in 'Contact person',           with: 'none'
     fill_in 'Manufacturer/Description', with: 'Asus'
     fill_in 'Model number',             with: 'XYZ'
@@ -22,7 +24,7 @@ describe 'New work order' do
     fill_in 'Physical condition',       with: 'good'
     fill_in 'Reported problem',         with: 'no power'
     check 'Under warranty'
-    select 'Test supplier'
+    select_from_chosen 'Test supplier', from: 'Supplier'
     fill_in 'Purchase date',            with: Date.current
     fill_in 'Expiry date',              with: Date.current.next_year
 
