5f10e092a24ebae20d50516cc81b95c778d9e33c
Von Christian <vc.halip@gmail.com>
2020-06-24 21:00:33 +0800

    Reorder work orders to show latest

--- app/controllers/computer_repair_section/dashboard_controller.rb
+++ app/controllers/computer_repair_section/dashboard_controller.rb
@@ -5,18 +5,18 @@ module ComputerRepairSection
       @to_date   = params[:to_date] || DateTime.now.end_of_month
       if params[:technician].present?
         @technician = User.find(params[:technician])
-       @pagy, @work_orders = pagy(@technician.work_orders.includes(:product_unit, :customer, :charge_invoice, :technician_work_orders, :store_front, :technicians =>[:avatar_attachment =>[:blob]]))
+       @pagy, @work_orders = pagy(@technician.work_orders.order(created_at: :desc).includes(:product_unit, :customer, :charge_invoice, :technician_work_orders, :store_front, :technicians =>[:avatar_attachment =>[:blob]]))
       elsif params[:search].present?
-        @pagy, @work_orders = pagy(current_business.work_orders.includes(:product_unit, :customer, :charge_invoice, :technician_work_orders, :store_front, :technicians =>[:avatar_attachment =>[:blob]]).text_search(params[:search]))
+        @pagy, @work_orders = pagy(current_business.work_orders.order(created_at: :desc).includes(:product_unit, :customer, :charge_invoice, :technician_work_orders, :store_front, :technicians =>[:avatar_attachment =>[:blob]]).text_search(params[:search]))
       elsif params[:section_id].present?
-        @pagy, @work_orders = pagy(Section.find(params[:section_id]).work_orders.includes(:product_unit, :customer, :charge_invoice, :technician_work_orders, :store_front, :technicians =>[:avatar_attachment =>[:blob]]))
+        @pagy, @work_orders = pagy(Section.find(params[:section_id]).work_orders.order(created_at: :desc).includes(:product_unit, :customer, :charge_invoice, :technician_work_orders, :store_front, :technicians =>[:avatar_attachment =>[:blob]]))
       else
-        @pagy, @work_orders = pagy(current_business.work_orders.includes(:product_unit, :customer, :charge_invoice, :technician_work_orders, :store_front, :technicians =>[:avatar_attachment =>[:blob]]))
+        @pagy, @work_orders = pagy(current_business.work_orders.order(created_at: :desc).includes(:product_unit, :customer, :charge_invoice, :technician_work_orders, :store_front, :technicians =>[:avatar_attachment =>[:blob]]))
       end
-      @pagy, @received_work_orders = pagy(current_business.work_orders.received)
-      @pagy, @work_in_progress_work_orders = pagy(current_business.work_orders.work_in_progress)
-      @pagy, @released_work_orders = pagy(current_business.work_orders.released)
-      @pagy, @done_and_rto_work_orders = pagy(current_business.work_orders.done_and_rto)
+      @pagy, @received_work_orders = pagy(current_business.work_orders.received.order(created_at: :desc))
+      @pagy, @work_in_progress_work_orders = pagy(current_business.work_orders.work_in_progress.order(created_at: :desc))
+      @pagy, @released_work_orders = pagy(current_business.work_orders.released.order(created_at: :desc))
+      @pagy, @done_and_rto_work_orders = pagy(current_business.work_orders.done_and_rto.order(created_at: :desc))
     end
   end
 end
